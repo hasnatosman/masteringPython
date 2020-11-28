b3b689ccc7f728f56edcc682926d2281a3807ad1
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-11-28 14:01:40 +0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,10 @@
+# Variable is like a bucket or container, that hold any data. you can put data, you can retrieve data from it.
+
+x = 5
+# here x is a variable and 5 is the value.
+rent = 550
+gas = 550
+total = rent + gas
+print(total)
+# NOTE: we can't use a keyword as a variable name.
+
