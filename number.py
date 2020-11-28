afb6951f440c70d692b5fd1666c70379715f95e9
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-11-28 14:01:57 +0600

    Add files via upload

---
+++ number.py
@@ -0,0 +1,16 @@
+# number is an ingredient of code.
+
+x = 10
+y = 3
+add = x + y
+sub = x - y
+mul = x * y
+div = x / y
+rem = x % y
+
+print(add)
+print(sub)
+print(mul)
+print(div)
+print(rem)
+print(round(div, 2))
