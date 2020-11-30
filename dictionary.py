73b1615ea9d3affb51ad500070a0121e9d4d918b
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-11-30 17:14:26 +0600

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+dict = {
+    'tom': 1234,
+    'joe': 3213,
+    'roy': 5432
+}
+"""
+for key in dict:
+    print('Key: ', key, '.', 'Value: ', dict[key])
+"""
+for k, v in dict.items():
+    print('Key: ', k, '...', 'Value: ', v)
