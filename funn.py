222228662513d1b48f294f9ddd1fec640188e440
Hasnat <61647150+hasnatosman@users.noreply.github.com>
2020-12-17 23:11:20 +0600

    Add files via upload
    just fun.

---
+++ funn.py
@@ -0,0 +1,7 @@
+num1 = int(input("First number: "))
+num2 = int(input("Second number: "))
+if (num2 >= num1):
+    largest = num2
+else:
+    largest = num1
+print("Largest number you entered is: ", largest)
